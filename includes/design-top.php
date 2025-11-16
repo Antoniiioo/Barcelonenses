3ed8493fca339d9d8412774f6cfe7f8ff8ea2961
Antonio <agravaz929@iesmarquesdecomares.org>
2025-11-16 12:08:18 +0100

    Incluir head, header, menu y footer

--- includes/design-top.php
+++ includes/design-top.php
@@ -1,5 +1,12 @@
-<header class="masthead bg-primary text-white text-center">
-        <div class="container d-flex align-items-center flex-column">
-            <p class="masthead-subheading font-weight-light mb-0">PHP + Bootstrap Miniframework</p>
+<header class="d-flex justify-content-between align-items-center px-4 py-2">
+        <div class="d-flex align-items-center gap-2">
+            <img src="assets/img/Logo.png" alt="Logo" width="60" height="60">
+            <h1 class="fs-3 m-0">R&V</h1>
         </div>
-</header>
+
+        <div class="d-flex align-items-center gap-4 fs-4">
+            <a href=""><span class="bi bi-person"></span></a>
+            <a href=""><span class="bi bi-cart"></span></a>
+            <a href=""><span class="bi bi-heart"></span></a>
+        </div>
+    </header>
--- includes/navigation.php
+++ includes/navigation.php
@@ -1,35 +1,31 @@
-<nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
-    <div class="container">
-        <a class="navbar-brand" href="index.php">Start Bootstrap</a>
-        <button class="navbar-toggler text-uppercase font-weight-bold text-white rounded" type="button"
-            data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive"
-            aria-expanded="false" aria-label="Toggle navigation">
-            <i class="fas fa-bars fa-2x"></i>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item dropdown">
-                    <a class="nav-link py-3 px-0 px-lg-3  dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <div class="dropdown-menu bg-secondary text-uppercase border-0 " aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item text-white" href="#">Action</a>
-                        <a class="dropdown-item text-white" href="#">Another action</a>
-                        <a class="dropdown-item text-white" href="#">Something else here</a>
-                    </div>
+<nav class="navbar navbar-expand-lg bg-white py-3">
+        <div class="container-fluid">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="">Ropa</a>
                 </li>
-                <li class="nav-item mx-0 mx-lg-1"><a
-                        class="nav-link <?php if ($CURRENT_PAGE == "Index") {?>active<?php }?> py-3 px-0 px-lg-3 rounded"
-                        href="index.php">Portfolio</a></li>
-                <li class="nav-item mx-0 mx-lg-1"><a
-                        class="nav-link <?php if ($CURRENT_PAGE == "About") {?>active<?php }?> py-3 px-0 px-lg-3 rounded"
-                        href="about.php">About</a></li>
-                <li class="nav-item mx-0 mx-lg-1"><a
-                        class="nav-link <?php if ($CURRENT_PAGE == "Contact") {?>active<?php }?> py-3 px-0 px-lg-3 rounded"
-                        href="contact.php">Contact</a></li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Calzado</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Accesorios</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="">Novedades</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Marcas</a>
+                </li>
             </ul>
+
+            <form class="d-flex">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Buscar..." aria-label="Buscar">
+
+                    <button class="btn btn-primary" type="submit">
+                        <span class="bi bi-arrow-right"></span>
+                    </button>
+                </div>
+            </form>
         </div>
-    </div>
-</nav>
+    </nav>
